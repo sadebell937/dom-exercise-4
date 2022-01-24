99f8151f787be0e9c7c50c41e707c22d588a9b6c
sadebell937 <93891161+sadebell937@users.noreply.github.com>
2022-01-24 18:34:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const money = document.getElementById('money');
+const number = document.getElementById('number');
+const button = document.getElementById('button');
+const button1 = document.querySelector('dotdime');
+
+button.addEventListener('click', (event) => {
+    console.log(event);
+    const totalamount = "number"
+    let amount = 0;
+    if (event.target.id === 'Dime') {
+        amount ="dotdime" + number;
+
+    }
+});
+
+
+
